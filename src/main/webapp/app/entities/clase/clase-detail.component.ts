e48eae253466d07235f4cafcf26a93a47e98fd57
Luz0601 <luzmarinaamoretti@gmail.com>
2022-12-22 17:21:29 +0100

    boton edit

--- src/main/webapp/app/entities/clase/clase-detail.component.ts
+++ src/main/webapp/app/entities/clase/clase-detail.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IClase } from 'app/shared/model/clase.model';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ClaseUpdateComponent } from './clase-update.component';
 
 @Component({
   selector: 'jhi-clase-detail',
@@ -11,11 +12,17 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 export class ClaseDetailComponent implements OnInit {
   clase: IClase;
 
-  constructor(protected activatedRoute: ActivatedRoute, protected modal: NgbActiveModal) {}
+  constructor(protected activatedRoute: ActivatedRoute, protected modal: NgbActiveModal, private modalService: NgbModal) {}
 
   ngOnInit() {}
 
   previousState() {
     window.history.back();
+  }
+
+  open(content) {
+    // this.modal.close('Edit');
+    const modalRef = this.modalService.open(ClaseUpdateComponent, { ariaLabelledBy: 'modal-basic-title' });
+    modalRef.componentInstance.clase = content;
   }
 }
